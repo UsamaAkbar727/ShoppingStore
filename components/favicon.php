a546012d45aedef43d3b77472e26038a5316fc72
Usama Akbar <usamaakbarjaatt@gmail.com>
2026-07-12 15:10:30 +0500

    feat: add site favicon and dynamic link component for browser branding

--- components/favicon.php
+++ components/favicon.php
@@ -5,6 +5,6 @@ $fav_path = (isset($base_url) && !empty($base_url)) ? $base_url . 'favicon/' : '
 ?>
 <link rel="icon" type="image/png" href="<?php echo $fav_path; ?>favicon-96x96.png" sizes="96x96" />
 <link rel="icon" type="image/svg+xml" href="<?php echo $fav_path; ?>favicon.svg" />
-<link rel="shortcut icon" href="<?php echo $fav_path; ?>favicon.ico" />
+<link rel="shortcut icon" href="<?php echo (isset($base_url) && !empty($base_url)) ? $base_url . 'favicon.ico' : '/favicon.ico'; ?>" />
 <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $fav_path; ?>apple-touch-icon.png" />
 <link rel="manifest" href="<?php echo $fav_path; ?>site.webmanifest" />
